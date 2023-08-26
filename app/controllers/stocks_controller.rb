cc11feaff90fc9cdb44e3c66bda5cbe835476aa3
Fernando Mendes <fernando.msantos1@gmail.com>
2023-08-26 18:53:16 -0300

    refatoring navbar, search logout

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -1,6 +1,7 @@
 class StocksController < ApplicationController
   def search
     @stock = Stock.new_lookup(params[:stock])
+    @tracked_stocks = current_user.stocks
     
     render 'users/my_portfolio'
   end
