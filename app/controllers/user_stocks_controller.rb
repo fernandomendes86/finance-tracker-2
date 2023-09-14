b12578454563dacffa41deb6c30190dd4563a38b
Fernando Mendes <fernando.msantos1@gmail.com>
2023-09-14 10:41:59 -0300

    delete stock from portfolio

--- app/controllers/user_stocks_controller.rb
+++ app/controllers/user_stocks_controller.rb
@@ -1,5 +1,6 @@
 class UserStocksController < ApplicationController
-  before_action :set_stock, only: [:create]
+  before_action :set_stock_by_ticker, only: [:create]
+  before_action :set_stock_by_id, only: [:destroy]
 
   def create 
     unless @stock
@@ -11,9 +12,22 @@ class UserStocksController < ApplicationController
     redirect_to my_portfolio_path
   end
 
+  def destroy
+    user_stock = UserStock.find_by(user_id: current_user, stock_id: @stock)
+    if user_stock
+      user_stock.destroy
+      flash[:notice] = "#{@stock.ticker} was successfully removed from portfolio"
+      redirect_to my_portfolio_path
+    end
+  end
+
   private
   
-  def set_stock
+  def set_stock_by_ticker
     @stock = Stock.find_by_ticker(params[:ticker])
+  end
+
+  def set_stock_by_id
+    @stock = Stock.find(params[:id])
   end
 end
