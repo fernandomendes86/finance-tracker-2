9196de0d5918a5d02c71986b0c6efac49eac4552
Fernando Mendes <fernando.msantos1@gmail.com>
2023-09-15 11:43:53 -0300

    create migration first name and last name to user

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,7 @@
 module UsersHelper
+  def print_full_name
+    return "Anonymous" unless current_user.first_name || current_user.last_name
+      
+    "#{current_user.first_name} #{current_user.last_name}"
+  end
 end
