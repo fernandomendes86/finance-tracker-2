bc91313d0224c0113ae80b60533ea5aab9c338b7
Fernando Mendes <fernando.msantos1@gmail.com>
2023-09-19 11:44:14 -0300

    add resource and views to friends

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   get 'welcome/index'
   get 'my_portfolio', to: 'users#my_portfolio'
   get 'search_stock', to: 'stocks#search'
+  get 'my_friends', to: 'users#my_friends'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
