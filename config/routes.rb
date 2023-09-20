7f86fb65d6c70a828227a76d4e63e9d8b2b1b1da
Fernando Mendes <fernando.msantos1@gmail.com>
2023-09-20 09:59:38 -0300

    search friend form

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,10 @@ Rails.application.routes.draw do
   devise_for :users
   get 'welcome/index'
   get 'my_portfolio', to: 'users#my_portfolio'
-  get 'search_stock', to: 'stocks#search'
   get 'my_friends', to: 'users#my_friends'
+  get 'search_friend', to: 'users#search'
+  get 'search_stock', to: 'stocks#search'
+  
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
