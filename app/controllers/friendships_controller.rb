a0d94d10d0829d7600f54736e8273dc70d0c070f
Fernando Mendes <fernando.msantos1@gmail.com>
2023-09-22 10:47:06 -0300

    implements action to follow a user

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -1,5 +1,15 @@
 class FriendshipsController < ApplicationController
+  before_action :set_friend, only: [:create]
+
   def create
+    friendship = current_user.friendships.build(friend: @friend)
+    if friendship.save
+      flash[:notice] = "Following user" 
+    else
+      flash[:alert] = "Unable to follow user"
+    end
+
+    redirect_to my_friends_path
   end
 
   def destroy
@@ -10,6 +20,12 @@ class FriendshipsController < ApplicationController
       
       redirect_to my_friends_path
     end
+  end
+
+  private
+
+  def set_friend
+    @friend = User.find(params[:friend])
   end
 
 end
